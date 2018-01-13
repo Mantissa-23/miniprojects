33cd958af4611f1dd04987b707b5a71c27be5035
Mantissa-23 <dylanculfogienis@gmail.com>
2018-01-13 08:58:43 -0500

    Playing around with arrays, more refershers

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <string.h>
+#define ARRAYSIZE(arr) sizeof(arr)/sizeof(arr[0])
+
 
 int main() {
-    printf("Hello world!");
+    int testarray[] = {1, 2, 3, 4, 5};
+
+    int i;
+
+    for(i=0; i < ARRAYSIZE(testarray); i++) {
+        printf("%d", testarray[i]);
+    }
+
+    printf("\n");
     return 0;
 }
