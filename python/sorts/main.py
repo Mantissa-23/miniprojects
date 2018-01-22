4ee607bc431f388eb215248954e73f1150629f7c
Mantissa <dylanculfogienis@gmail.com>
2018-01-22 12:41:17 -0500

    Added a main.py that tests all sort functions.

---
+++ python/sorts/main.py
@@ -0,0 +1,19 @@
+import bubblesort
+import insertionsort
+import mergesort
+import quicksort
+import selectionsort
+
+from utils import test
+
+if __name__=="__main__":
+    testfuncs = [
+        bubblesort.bubblesort,
+        insertionsort.insertionsort,
+        mergesort.mergesort,
+        quicksort.quicksort,
+        selectionsort.selectionsort
+    ]
+
+    for function in testfuncs:
+        test(function, verbose=False)
