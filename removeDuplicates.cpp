d94b11faa63fff82ff9d07341ecdda1d2a598756
Mantissa-23 <dylanculfogienis@gmail.com>
2020-11-08 14:12:42 -0500

    Fixed bug with removeDuplicates

--- removeDuplicates.cpp
+++ removeDuplicates.cpp
@@ -7,6 +7,9 @@ using namespace std;
 class Solution {
 public:
   int removeDuplicates(vector<int> &nums) {
+    if(nums.size() == 0)
+      return 0;
+
     int i = 0;
     for(int j = 0; j < nums.size(); j++) {
       if(nums[i] != nums[j]) {
@@ -35,6 +38,10 @@ TEST_CASE("tests") {
   vector<int> arr;
   vector<int> soln;
 
+  SUBCASE("0") {
+    arr = {};
+    soln = {};
+  }
   SUBCASE("1") {
     arr = {1, 1, 2, 2, 3, 3, 3, 4, 4, 4, 4};
     soln = {1, 2, 3, 4};
@@ -46,6 +53,10 @@ TEST_CASE("tests") {
   SUBCASE("3") {
     arr = {0, 7, 11, 13, 13, 28, 28, 28, 55, 77, 77, 77};
     soln = {0, 7, 11, 13, 28, 55, 77};
+  }
+  SUBCASE("4") {
+    arr = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+    soln = {1};
   }
 
   CHECK(s.removeDuplicates(arr) == soln.size());
