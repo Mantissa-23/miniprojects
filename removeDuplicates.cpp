bbba11f7e453fc88a03687b256578261b6878def
Mantissa-23 <dylanculfogienis@gmail.com>
2020-11-08 14:10:00 -0500

    Did this one

--- removeDuplicates.cpp
+++ removeDuplicates.cpp
@@ -1,57 +1,53 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <unordered_map>
 #include "doctest.h"
-#include <vector>
 
 using namespace std;
 
 class Solution {
 public:
-    int removeDuplicates(vector<int>& nums) {
-        if(nums.size() < 2)
-            return nums.size();
-        
-        int i = 1;
-        for (int j = 1; j < nums.size(); j++) {
-            if (nums[j] != nums[j - 1]) {
-                nums[i] = nums[j];
-                i++;
-            }
-        }
-        
-        return i;
+  int removeDuplicates(vector<int> &nums) {
+    int i = 0;
+    for(int j = 0; j < nums.size(); j++) {
+      if(nums[i] != nums[j]) {
+        i++;
+        nums[i] = nums[j];
+      }
     }
+    return i + 1;
+  }
 };
 
-template<typename T>
-bool isEqual(std::vector<T> const &v1, std::vector<T> const &v2, int len)
-{
-  for (int i = 0; i < len; i++)
-    if(v1[i] != v2[i])
+bool isEqual(vector<int> arr1, vector<int> arr2) {
+  if(arr1.size() != arr2.size())
+    return false;
+
+  for(int i = 0; i < arr1.size(); i++) {
+    if(arr1[i] != arr2[i])
       return false;
+  }
   return true;
 }
 
-TEST_CASE("solution") {
+TEST_CASE("tests") {
   Solution s;
 
-  vector<int> t;
-  vector<int> tt;
-  int expected_len;
+  vector<int> arr;
+  vector<int> soln;
 
-  SUBCASE("Short") {
-    t = {1,1,2};
-    tt = {1, 2};
-  }
-  SUBCASE("Long") {
-    t = {0,0,1,1,1,2,2,3,3,4};
-    tt = {0,1,2,3,4};
-  }
   SUBCASE("1") {
-    t = {0,0,0,3,3,4,4,4,4,4,8,8,10,12,20};
-    tt = {0,3,4,8,10,12,20};
+    arr = {1, 1, 2, 2, 3, 3, 3, 4, 4, 4, 4};
+    soln = {1, 2, 3, 4};
+  }
+  SUBCASE("2") {
+    arr = {0, 0, 0, 1, 1, 2, 3, 3, 3, 4, 4};
+    soln = {0, 1, 2, 3, 4};
+  }
+  SUBCASE("3") {
+    arr = {0, 7, 11, 13, 13, 28, 28, 28, 55, 77, 77, 77};
+    soln = {0, 7, 11, 13, 28, 55, 77};
   }
 
-  int len = s.removeDuplicates(t);
-  CHECK(len == tt.size());
-  CHECK(isEqual(t, tt, len));
+  CHECK(s.removeDuplicates(arr) == soln.size());
+  isEqual(arr, soln);
 }
