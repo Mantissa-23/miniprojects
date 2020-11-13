a567b4d2530c8b7c8bd9a05161dc7ee5ffc2e0dd
Mantissa-23 <dylanculfogienis@gmail.com>
2020-11-12 19:45:50 -0500

    Minimal working version

--- clojure-projects/src/clojure_projects/vignere.clj
+++ clojure-projects/src/clojure_projects/vignere.clj
@@ -1,20 +1,9 @@
-(def u clojure.string/upper-cse)
+(def u clojure.string/upper-case)
 (def i #(- (int %) 65))
-(def c #(char (+ % 65)))
-(def s #(apply str (map %1 %2 %3)))
-(defn g [o] #(c (mod (o %1 %2) 26)))
-
 (defn a [m k f]
   (let [r (map i (u m))
         v (map i (cycle (u k)))]
-    (f r v)))
-
-(defn e [m k]
-  (a m k  #(s (g +) %1 %2)))
-
-(defn d [m k]
-  (a m k  #(s (g -) %1 %2)))
-
+    (apply str (map #(char (+ (mod (f %1 %2) 26) 65)) r v))))
 (let [[m k] *command-line-args*
-      z (e m k)]
-  (apply println [z (d z k)]))
+      z (a m k +)]
+  (println [z (a z k -)]))
